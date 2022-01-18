1f24ba74684cb49c266e749146134fb95135c713
Evan Walsh <60162028+Evan-Walsh1@users.noreply.github.com>
2022-01-18 15:03:57 -0500

    good

--- filesize.cc
+++ filesize.cc
@@ -1,5 +1,14 @@
+/**************************************************
+ * Name: Evan Walsh
+ * Class: CS4000
+ * Professor: Ostermann
+ * ************************************************/
+
+
+
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 // output format to use IDENTICALLY:
@@ -7,12 +16,28 @@ using namespace std;
 //program: ./filesize
 // input.3lines: 3
 // input.1line: 1
-
+//
 int main( int argc, char* argv[] )
-{
+{   
+    cout << "program: " << argv[0] << endl;
     // just to get you started, this is how to refer to the arguments that were passed
-    for (int arg = 0; arg < argc; ++arg)
-            std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
-
+    for (int arg = 1; arg < argc; ++arg){
+            //std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
+        ifstream input; //Reads in file
+        input.open(argv[arg]); //opens file
+        if(input){
+            int line_count = 0; //Sets count to 0
+            string s;
+            while(input){
+                getline(input, s);
+                line_count++; //increments count by 1
+            }
+        
+            cout << " " << argv[arg] << ": " << line_count -1 << endl; //prints out each argument and the # of lines it has
+        }else{
+            cout << " " << argv[arg] << ": -1" << endl; //prints out -1 if the file can not be open
+        }
+    }
+    
     exit(0); // this means that the program executed correctly!
 }
